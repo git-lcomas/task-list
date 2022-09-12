2cb218578ab382e675779241ecd84d04087c4ccf
Luis <lcomas@hotmail.com>
2022-09-11 23:14:30 -0300

    servicio y servidor de tareas listo

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+
+import { TaskService } from '../../service/task.service';
 import {Task} from '../../Task'
-import {TASKS} from '../../mock-tasks'
-
-
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
-  }
-
+    // like promise
+    this.taskService.getTasks().subscribe((tasks)=>{
+      this.tasks = tasks
+    });
+    }
 }
--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -5,25 +5,25 @@ export const TASKS: Task[] = [
         id: 1,
         text: "Terminar primer modulo de Angular",
         day: "Agosto 5 a las 12:00",
-        reminder: true,
+        reminder: true
     },
     {
         id: 2,
         text: "Hacer las compras para la cena",
         day: "Agosto 5 a las 17:00",
-        reminder: true,
+        reminder: true
     },
     {
         id: 3,
         text: "Investigar sobre Bootstrap",
         day: "Agosto 5 a las 19:00",
-        reminder: false,
+        reminder: false
     },
     {
         id: 4,
         text: "Leer el libro favorito",
         day: "Agosto 5 a las 21:00",
-        reminder: false,
+        reminder: false
     },
     
 ]
